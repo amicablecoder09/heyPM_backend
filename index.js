88292250ad28f783d2f9c5d23d47347589e7fe51
amicablecoder09 <ankit.guddewala@gmail.com>
2021-01-18 23:14:50 +0530

    login signup code

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const cors = require('cors');
-
+const pool = require("./src/db");
 const app = express();
 
+app.use(express.json()); // this will help to do req.body
 app.use(cors());
 
-app.get('/', function (req, res) {
+//routes
+
+// register and login routes
+app.use("/auth", require("./routes/jwtAuth"))
+
+// dashboard route
+app.use("/dashboard",require("./routes/dashboard"));
+
+app.put('/getcontentdata', async (req, res) => {
+  try {
+    let client = await pool.connect();
+    //await client.query(
+      //`SELECT FROM CONTENT ()`
+    //);
+  } catch (e) {
+    return res.status(500).send({ errorMessage: "Internal Server Error " + e.message });
+  }
   res.send('Hello World!');
 });
 
